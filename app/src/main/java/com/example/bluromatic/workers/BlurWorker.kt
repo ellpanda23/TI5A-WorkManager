2f984eb48e42e1ddf326cfb83a0908feb5f032d7
ellpanda23 <s20120157@alumnos.itsur.edu.mx>
2024-02-08 23:02:05 -0600

    10. Datos de entrada y datos de salida

--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -2,8 +2,12 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import com.example.bluromatic.workers.blurBitmap
 import com.example.bluromatic.workers.makeStatusNotification
@@ -16,23 +20,32 @@ private const val TAG = "BlurWorker"
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
-        makeStatusNotification(
-            applicationContext.resources.getString(R.string.blurring_image),
-            applicationContext
-        )
+
+        // ADD THESE LINES
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
         //return withContext(Dispatchers.IO) try {
         return withContext(Dispatchers.IO) {
             //return try {
             return@withContext try{
 
-                delay(DELAY_TIME_MILLIS)
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
 
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
+                val resolver = applicationContext.contentResolver
+
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
                 )
 
-                val output = blurBitmap(picture, 1)
+                delay(DELAY_TIME_MILLIS)
+
+                val output = blurBitmap(picture, blurLevel)
 
                 // Write bitmap to a temp file
                 val outputUri = writeBitmapToFile(applicationContext, output)
@@ -42,7 +55,9 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
                     applicationContext
                 )
 
-                Result.success()
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
